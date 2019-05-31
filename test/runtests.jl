56758c5ba37a5e093d8653b172b2bf62f4be6f9a
Elliot Saba <staticfloat@gmail.com>
2019-05-31 01:40:46 -0700

    Fix `docker build` name

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Test
 container_id = nothing
 try
     # First, build the test server
-    success(`docker build -t influxdbjl_test_server influx_test_instance`)
+    success(`docker build -t influxdbjl_test_server influxdb_test_instance`)
 
     # Next, run it in the background
     global container_id = chomp(String(read(`docker run -d --rm -p 8086:8086 influxdbjl_test_server`)))
